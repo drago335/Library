a32c02735d2f763adc9b219e1ad6fdae200c7e6e
drago335 <drageto123@abv.bg>
2026-05-05 14:00:11 +0300

    feat: create Book model with constructor and getters/setters

---
+++ src/main/java/models/Book.java
@@ -0,0 +1,57 @@
+package models;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isAvailable;
+
+    public Book(int id, String title, String author, String isbn, boolean isAvailable) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+}
